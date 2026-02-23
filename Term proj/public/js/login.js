c5f783fb3e12ba90257070eae1daf135e5d0eaf8
SuperOmegaJuan <justgimmesomenamethaticanuse@gmail.com>
2026-02-23 13:40:07 +0700

    Update login.js
    Fixed Login bug

--- Term proj/public/js/login.js
+++ Term proj/public/js/login.js
@@ -1,17 +1,11 @@
+const loginBtn = document.querySelector(".login-button");
 const modal = document.getElementById("loginModal");
-const openBtn = document.querySelector(".user-icon");
 const closeBtn = document.querySelector(".close-btn");
 
-openBtn.onclick = () => {
+loginBtn.onclick = () => {
   modal.style.display = "flex";
 };
 
 closeBtn.onclick = () => {
   modal.style.display = "none";
-};
-
-window.onclick = (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
 };
